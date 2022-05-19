e936e5988b5316bdf51895c5cf10aa8642854a7d
胡维龙 <weilong.hu@amh-group.com>
2022-05-19 17:03:57 +0800

    1. 清除分支，专门用于存储文件

--- gravity-plugin-api/src/main/java/io/manbang/gravity/plugin/EmptyAdvice.java
+++
@@ -1,9 +0,0 @@
-package io.manbang.gravity.plugin;
-
-/**
- * @author duoliang.zhang
- * @since 2020/11/5 13:57
- */
-enum EmptyAdvice implements Advice {
-    INSTANCE
-}
--- gravity-plugin-api/src/main/java/io/manbang/gravity/plugin/EmptyInterceptor.java
+++
@@ -1,9 +0,0 @@
-package io.manbang.gravity.plugin;
-
-/**
- * @author duoliang.zhang
- * @since 2020/11/5 13:58
- */
-enum EmptyInterceptor implements Interceptor {
-    INSTANCE
-}
